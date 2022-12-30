f6a13b5a0083c74aba6e320ca0a8aaf5228795bb
Juan Santa <jcsantahurtado@gmail.com>
2022-12-29 23:21:46 -0500

    runner separation

---
+++ src/test/java/com/sophos/academy/runners/AgregarRegistro.java
@@ -0,0 +1,18 @@
+package com.sophos.academy.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        glue = "com.sophos.academy.steps",
+        features = "src/test/resources/com.sophos.academy.features/agregar_registro.feature",
+        snippets = SnippetType.CAMELCASE,
+        monochrome = true,
+        dryRun = false
+)
+
+public class AgregarRegistro {
+}
--- src/test/java/com/sophos/academy/runners/InteractuarVentanas.java
+++ src/test/java/com/sophos/academy/runners/InteractuarVentanas.java
@@ -8,11 +8,11 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         glue = "com.sophos.academy.steps",
-        features = "src/test/resources/com.sophos.academy.features",
+        features = "src/test/resources/com.sophos.academy.features/interactuar_ventanas.feature",
         snippets = SnippetType.CAMELCASE,
         monochrome = true,
         dryRun = false
 )
 
-public class DemoQA {
+public class InteractuarVentanas {
 }
